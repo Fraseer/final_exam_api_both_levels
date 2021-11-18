a9729e954aceb1e27b5b7982d7e5a440031e2b17
Fraseer <Fraser.Hughes@btinternet.com>
2021-11-18 13:20:16 +0100

    adds sad path tests for no content in comment body

--- spec/requests/api/comments/create_spec.rb
+++ spec/requests/api/comments/create_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe 'POST /api/comments', type: :request do
 
   describe 'succesful creation of comment' do
     it 'is expected to return a 201 response' do
-      expect(response).to have_http_status 201   
+      expect(response).to have_http_status 201
     end
 
     it 'is expected that return a success message' do
@@ -24,6 +24,22 @@ RSpec.describe 'POST /api/comments', type: :request do
 
     it 'is expected that the user will have one comment on their account' do
       expect(user.comments.count).to eq 1
+    end
+  end
+
+  describe 'unsuccesful creation of comment' do
+    describe 'when the body has no content' do
+      it 'is expected to return a 422 response' do
+        expect(response).to have_http_status 422
+      end
+
+      it 'is expected to render a unsuccessful message' do
+        expect(response_json['error']).to eq 'You must enter some text to leave a comment'
+      end
+
+      it 'is expected that there will not be a comment on the article' do
+        expect(article.comments.count).to eq 0
+      end
     end
   end
 end
